65c001079f66d949b71c46dd002e7d4fb065f68c
arjun-901 <arjun9654abc@gmail.com>
2025-08-26 01:10:36 +0530

    chage backend and frontend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://digi-soch-chat-app-full-stack-mern.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
