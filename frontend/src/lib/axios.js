c36c685ba33365db07c5a64abbebc4ec0821b2a0
Arjun Maurya <arjun9654abc@gmail.com>
2025-08-30 18:09:40 +0530

    Update axios.js
    fir se axios chage

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
+import axios from "axios";
+
 const BASE_URL = "https://digi-soch-chat-app-full-stack-mern-mauve.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
+  withCredentials: true, // send cookies with the request
 });
