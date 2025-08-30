936caba5720f8b114231177820f4917ab33b272f
Arjun Maurya <arjun9654abc@gmail.com>
2025-08-30 18:03:43 +0530

    Update axios.js
    chage axios file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
-import axios from "axios";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "https://digi-soch-chat-app-full-stack-mern-mauve.vercel.app/api" : "/api";
+const BASE_URL = "https://digi-soch-chat-app-full-stack-mern-mauve.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  withCredentials: true,
 });
